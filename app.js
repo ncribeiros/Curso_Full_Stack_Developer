0d092322fc1f0fe0d163f853726f18259d5f7ae7
Nathanael Ribeiro <ncribeiros@gmail.com>
2023-02-11 20:49:01 -0300

    Iniciando programação com JavaScript

--- app.js
+++ app.js
@@ -13,14 +13,57 @@ Tipos de Variáveis
 # string = texto
 # boolean = falso e verdadeiro (false or true)
 
+Operadores Lógicos
+
+# soma = +
+# subtração = -
+# divisão = /
+# multiplicação = *
+# resto = %
+# maior que = >
+# menor que = <
+# igualdade = +++
+
 */
 
-const meunome = "Nathanael Barbosa Ribeiro"
+/*const meunome = "Nathanael Barbosa Ribeiro"
 const minhaidade = "36"
 const ehlegal = true
+
 
 meunome = "Theo"
 
 console.log(meunome);
 console.log(minhaidade);
 console.log(ehlegal);
+*/
+
+let resultado = 10 + 10;
+console.log("Soma: ", resultado)
+
+resultado = 10 - 10;
+console.log("Subtração: ", resultado)
+
+resultado = 10 / 10;
+console.log("Divisão: ", resultado)
+
+resultado = 10 * 10;
+console.log("Multiplicação: ", resultado)
+
+resultado = 10 % 3;
+console.log("Resto: ", resultado)
+
+resultado = 4 < 5
+console.log("4 eh menor que 5?", resultado);
+
+resultado = 4 > 5
+console.log("4 eh maior que 5?", resultado);
+
+const valor1 = 10;
+const valor2 = 10;
+
+if (valor1 * valor2 === 100) {
+    console.log ("Valor é igual a 100");
+} else {
+    console.log ("Valor não é igual a 100");
+}
