5d16e8536e265c82545c445373534830584d8c42
Nathanael Ribeiro <ncribeiros@gmail.com>
2023-02-11 20:05:40 -0300

    Criação do Arquivo app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+/* console.log("Meu Primeiro Código JavaScript")
+
+/*
+Variáveis
+
+# var
+# let
+# const 
+
+Tipos de Variáveis
+
+# number = numérico
+# string = texto
+# boolean = falso e verdadeiro (false or true)
+
+*/
+
+const meunome = "Nathanael Barbosa Ribeiro"
+const minhaidade = "36"
+const ehlegal = true
+
+meunome = "Theo"
+
+console.log(meunome);
+console.log(minhaidade);
+console.log(ehlegal);
